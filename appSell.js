ac4b18196d79f23eecffd7a931d33817bc3baa7c
hooli nova <hoolinovaltd@gmail.com>
2023-08-23 11:23:38 +0300

    Node app Buy and Sell script

---
+++ appSell.js
@@ -0,0 +1,49 @@
+var NT = require("ninjatrader");
+
+var nt = new NT.default({
+    account: "Sim101"
+});
+
+function ntMarket(qty,instrument,action,cb){
+    nt.market({
+        action: NT.NinjaTraderAction.Sell,
+        quantity: qty,
+        tif: NT.NinjaTraderTif.Day,
+        instrument: instrument,
+        onUpdate: function (status, state) {
+            switch (status) {
+                case NT.OrderStatus.Rejected:
+                    // order rejected
+                    cb({
+                        stat:"rejected"
+                    })
+                    break;
+                case NT.OrderStatus.Filled:
+                    // order filled
+                    //console.log(state); // { quantity: 69, price: 49.40 }
+                    cb({
+                        stat:"filled",
+                        state:state
+                    })
+                    break;
+            }
+        },
+        onRejected: function (state) {
+            // order rejected
+            cb({
+                stat:"rejected",
+                state:state
+            })
+        }
+    }).then(()=>{
+        process.exit()
+    }); // order accepted after await
+}
+
+
+ntMarket(4,"NQ 09-23",NT.NinjaTraderAction.Sell,(res)=>{
+    console.log(res);
+});
+
+
+ console.log("Sell")
